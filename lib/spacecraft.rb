fa3bc726bbb25ff50bf73fdd5636282032f370b3
Antonio King <antonio.j.king@gmail.com>
2021-06-03 09:46:00 -0600

    Create add_requirement test to spacecraft class

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,9 +1,11 @@
 class Spacecraft
   attr_reader :name,
-              :fuel
-              
+              :fuel,
+              :requirements
+
   def initialize(spacecraft_data)
-    @name = spacecraft_data[:name]
-    @fuel = spacecraft_data[:fuel]
+    @name         = spacecraft_data[:name]
+    @fuel         = spacecraft_data[:fuel]
+    @requirements = []
   end
 end
--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -17,6 +17,19 @@ describe Spacecraft do
   end
 
   it 'adds requirements to spacecraft' do
+    craft_daed = {
+      name: 'Daedalus',
+      fuel: 400
+    }
 
+    daedalus = Spacecraft.new(craft_daed)
+
+    req_1 = {astrophysics: 6}
+    req_2 = {quantum_mechanics: 3}
+
+    daedalus.add_requirement(req_1)
+    daedalus.add_requirement(req_2)
+
+    expect(daedalus.requirements).to eq([req_1, req_2])
   end
 end
