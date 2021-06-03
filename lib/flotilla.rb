8d6a38248ee818aa520309237e6021d32a38eb3b
Antonio King <antonio.j.king@gmail.com>
2021-06-03 10:03:54 -0600

    Create add_personnel test for flotilla class

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -2,10 +2,14 @@ class Flotilla
   attr_reader :name,
               :personnel,
               :ships
-              
+
   def initialize(designation)
     @name      = designation[:designation]
     @personnel = []
     @ships     = []
   end
+
+  def add_ship(spacecraft)
+    @ships << spacecraft
+  end 
 end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -27,4 +27,33 @@ describe Flotilla do
 
     expect(seventh_flotilla.ships).to eq([daedalus])
   end
+
+  it 'adds personnel to designation' do
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+
+    kathy = Person.new('Kathy Chan', 10)
+    kathy.add_specialty(:astrophysics)
+    kathy.add_specialty(:quantum_mechanics)
+
+    polly = Person.new('Polly Parker', 8)
+    polly.add_specialty(:operations)
+    polly.add_specialty(:maintenance)
+
+    rover = Person.new('Rover Henriette', 1)
+    rover.add_specialty(:operations)
+    rover.add_specialty(:maintenance)
+
+    sampson = Person.new('Sampson Edwards', 7)
+    sampson.add_specialty(:astrophysics)
+    sampson.add_specialty(:quantum_mechanics)
+
+    seventh_flotilla.add_personnel(kathy)
+    seventh_flotilla.add_personnel(polly)
+    seventh_flotilla.add_personnel(rover)
+    seventh_flotilla.add_personnel(sampson)
+
+    employees = [kathy, polly, rover, sampson]
+
+    expect(seventh_flotilla.personnel).to eq(employees)
+  end
 end
